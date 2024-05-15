10367d926f8e414c6f7f8a307f198be9189a8b3f
Oswaldo Baptista Vicente Junior <45291656+oswaldobapvicjr@users.noreply.github.com>
2024-05-14 22:57:40 -0300

    Introduce new ExceptionMatcher.with(...) (#33)
    * Introduce new ExceptionMatcher.with(...)
    * Add Javadoc

---
+++ src/main/java/net/obvj/junit/utils/matchers/MyCustomException.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2024 obvj.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.obvj.junit.utils.matchers;
+
+public class MyCustomException extends Exception
+{
+    private static final long serialVersionUID = 5725758277733721926L;
+
+    private final String customString;
+    private final int code;
+
+    public MyCustomException(String message, String customString, int code)
+    {
+        super(message);
+        this.customString = customString;
+        this.code = code;
+    }
+
+    public String getCustomString()
+    {
+        return customString;
+    }
+
+    public int getCode()
+    {
+        return code;
+    }
+
+}
